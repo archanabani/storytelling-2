8abbb451415b1425cad0a81b490246e83754297b
archanabani <72745378+archanabani@users.noreply.github.com>
2024-05-29 19:27:21 +0530

    Update Feed.js

--- Feed.js
+++ Feed.js
@@ -51,8 +51,25 @@ export default class Feed extends Component {
       return (
         <View style={styles.container}>
           <SafeAreaView style={styles.droidSafeArea} />
-         
-     
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Storytelling App</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={stories}
+              renderItem={this.renderItem}
+            />
+          </View>
+          <View style={{ flex: 0.08 }} />
         </View>
       );
     }
@@ -60,5 +77,37 @@ export default class Feed extends Component {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cardContainer: {
+    flex: 0.85
+  }
 });
